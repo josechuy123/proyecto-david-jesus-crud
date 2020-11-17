faa284adafdbd370353a226f90cd6cec57d47904
dloredo <dloredo@ucol.mx>
2020-11-17 16:46:13 -0600

    Falta login

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 //let bd = { datos: [] }
 let bd = JSON.parse(localStorage.getItem("usuarios"));
-
-if (!bd || bd==undefined){
+if (!bd || bd==undefined)
+{
 	bd = { datos: []  }
 }
 
 let listar_usuarios = document.getElementById("listar_usuarios");
+let listar_usuarios_cobros = document.getElementById("listar_usuarios_cobros");
 
 document.getElementById("btnGuardarCliente").addEventListener("click" , ()=> {
     let nombre = document.getElementById("nombre").value;
@@ -13,34 +14,124 @@ document.getElementById("btnGuardarCliente").addEventListener("click" , ()=> {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     let rol = document.getElementById("rol").value;
+    let deuda = 0;
 
-    let datos = new Usuarios(nombre,telefono,email,password,rol);
+    let datos = new Usuarios(nombre,telefono,email,password,rol,parseInt(deuda));
     bd.datos.push(datos);
 
     localStorage.setItem("usuarios" , JSON.stringify(bd));
-    console.log(datos);
     location.replace("index.html");
 });
 
-function lista_usuarios(){
+function lista_usuarios()
+{
     let usuarios = JSON.parse(localStorage.getItem("usuarios"));
     listar_usuarios.innerHTML = "";
+    listar_usuarios_cobros.innerHTML = "";
     if(usuarios!=null)
     {
         usuarios.datos.forEach(usuario => {
             listar_usuarios.innerHTML+=`
             
-            <tr>
+            <tr style="text-align:center">
                 <th scope="row">${ usuario.nombre }</th>
                 <td>${ usuario.telefono }</td>
                 <td>${ usuario.email }</td>
+                <td>${ usuario.rol }</td>
+                <td>$${ usuario.deuda }</td>
                 <td>
                     <button type="button" class="btn btn-primary">Agregar cobro</button>
-                    <button type="button" class="btn btn-danger">Eliminar</button>
+                    <button type="button" class="btn btn-danger" onclick=" eliminar(${ usuario.telefono }) " >Eliminar</button>
                 </td>
             </tr>
            
             `
-        });      
+            listar_usuarios_cobros.innerHTML+=`
+            
+            <tr style="text-align:center">
+                <th scope="row">${ usuario.nombre }</th>
+                <td>${ usuario.telefono }</td>
+                <td>$${ usuario.deuda }</td>
+                <td>
+                    <button type="button" class="btn btn-success" onclick="abrirModalCliente(${ usuario.telefono })" >Cantidad a cobrar</button>
+                </td>
+            </tr>
+           
+            `
+        });    
+         
     }  
 }
+
+function eliminar(telefono)
+{
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")); 
+    console.log(usuarios);
+    usuarios.datos.forEach((usuario,i) => {
+        if( usuario.telefono == telefono )
+        {
+            usuarios.datos.splice(i,1);
+        }
+    });
+
+    localStorage.setItem('usuarios',JSON.stringify(usuarios));
+    location.replace("index.html");
+}
+
+function abrirModalCliente(telefono)
+{
+    $("#telefono_c").val(telefono)
+    $("#cantidad_cobrar_cliente").modal("show");
+}
+
+function abrirModalClientes()
+{
+    $("#cantidad_cobrar_clientes_todos").modal("show");
+}
+
+function AgregarDeudaClientes()
+{
+    let deuda = document.getElementById("deuda").value;
+    if(deuda <= 0 )
+        return alert("Numero mayor a 0");
+    console.log(deuda)
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")); 
+    
+    usuarios.datos.forEach((usuario) => {
+        usuario.deuda = parseInt(usuario.deuda)+parseInt(deuda);
+        console.log(usuario.deuda)
+    });
+    
+    localStorage.setItem('usuarios',JSON.stringify(usuarios));
+    location.replace("index.html");
+}
+
+//Agregar deuda a un solo cliente en especial
+function AgregarDeudaCliente()
+{
+    let telefono = document.getElementById("telefono_c").value;
+    let deuda = document.getElementById("deuda").value;
+    if(deuda <= 0 )
+        return alert("Numero mayor a 0");
+    console.log(deuda)
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")); 
+    
+    usuarios.datos.forEach((usuario) => {
+        if( usuario.telefono == telefono )
+        {
+            usuario.deuda = parseInt(usuario.deuda)+parseInt(deuda);
+            console.log(usuario.deuda)
+        }
+    });
+    
+    localStorage.setItem('usuarios',JSON.stringify(usuarios));
+    location.replace("index.html");
+}
+
+//Agregar deuda a todos los clientes por igual
+
+//Agregar deuda a todos los clientes por igual
+document.getElementById("BtnAgregarDeudaClientes").addEventListener("click" , ()=> {
+    console.log("Agregar deuda a todos los clientes por igual");
+});
+
--- js/usuarios.js
+++ js/usuarios.js
@@ -1,9 +1,10 @@
 class Usuarios {
-    constructor(nombre,telefono,email,password,rol){
+    constructor(nombre,telefono,email,password,rol,deuda){
         this.nombre = nombre;
         this.telefono = telefono;
         this.email = email;
         this.password = password;
+        this.deuda = deuda;
         this.rol = rol;
     }
 }
